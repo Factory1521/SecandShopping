43927eb65c4fc546aa1a07c5df32ea4bf562522e
Factory1521 <103991026+Factory1521@users.noreply.github.com>
2022-05-01 11:02:52 +0800

    Add files via upload
    文件上传功能

---
+++ springbootdemo/src/main/java/edu/hdc/springbootdemo/springbootdemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package edu.hdc.springbootdemo.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public void hello(){
+        System.out.println("hello");
+    }
+}
